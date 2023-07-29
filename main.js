78bb37c6680a0e531a1518613dcd395dc7c01458
Donovan Rojas <rojasdrm03@gmail.com>
2023-07-29 12:27:51 -0500

    upload in real time

--- main.js
+++ main.js
@@ -29,11 +29,11 @@
   // circle.bindPopup("¡Hola! Soy un círculo.");
   // polygon.bindPopup("¡Hola! Soy un polígono.");
 
-const ISS_TLE = 
+const PlatziSat_01 = 
   `1 88888U 24001FA  23163.94096086  .00000000  00000-0  10000-4 0  9999
   2 88888  97.5077 280.5424 0008220 228.6198 130.8530 15.11803180  1009`;
-const tl1 = ISS_TLE.split('\n')[0].trim();
-const tl2 = ISS_TLE.split('\n')[1].trim();
+const tl1 = PlatziSat_01.split('\n')[0].trim();
+const tl2 = PlatziSat_01.split('\n')[1].trim();
 console.log(tl1);
 console.log(tl2);
 
@@ -74,16 +74,50 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
 viewer.scene.globe.enableLighting = true;
 
 //Finally, we'll visualize the satellite position as a red dot in space:
+// const satellitePoint = viewer.entities.add({
+//   position: Cesium.Cartesian3.fromRadians(
+//     position.longitude, 
+//     position.latitude, 
+//     position.height * 1000
+//   ),
+//   point: { 
+//     pixelSize: 5, 
+//     color: Cesium.Color.RED }
+// });
+
+// Give SatelliteJS the TLE's and a specific time.
+// Get back a longitude, latitude, height (km).
+// We're going to generate a position every 10 seconds from now until 6 seconds from now. 
+const totalSeconds = 60 * 60 * 6;
+const timestepInSeconds = 10;
+const start = Cesium.JulianDate.fromDate(new Date());
+const stop = Cesium.JulianDate.addSeconds(start, totalSeconds, new Cesium.JulianDate());
+viewer.clock.startTime = start.clone();
+viewer.clock.stopTime = stop.clone();
+viewer.clock.currentTime = start.clone();
+viewer.timeline.zoomTo(start, stop);
+viewer.clock.multiplier = 40;
+viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP;
+
+//Creamos un SampledPositionProperty. Se trata de un objeto que contendrá muestras de posición a lo largo del tiempo e interpolará entre ellas
+const positionsOverTime = new Cesium.SampledPositionProperty();
+// Hacemos un bucle a través de cuantas muestras queramos obtener, y para cada muestra, construimos un objeto de tiempo, llamado JulianDate en CesiumJS, y una posición, y lo añadimos como una muestra::
+for (let i = 0; i < totalSeconds; i+= timestepInSeconds) {
+  const time = Cesium.JulianDate.addSeconds(start, i, new Cesium.JulianDate());
+  const jsDate = Cesium.JulianDate.toDate(time);
+
+  const positionAndVelocity = satellite.propagate(satrec, jsDate);
+  const gmst = satellite.gstime(jsDate);
+  const p   = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
+  // ...Get position from satellite-js...
+  const position = Cesium.Cartesian3.fromRadians(p.longitude, p.latitude, p.height * 1000);
+  positionsOverTime.addSample(time, position);
+}
+// Por último, pasamos este objeto positionsOverTime a nuestro punto
 const satellitePoint = viewer.entities.add({
-  position: Cesium.Cartesian3.fromRadians(
-    position.longitude, 
-    position.latitude, 
-    position.height * 1000
-  ),
-  point: { 
-    pixelSize: 5, 
-    color: Cesium.Color.RED }
+  position: positionsOverTime,
+  point: { pixelSize: 5, color: Cesium.Color.YELLOW }
 });
-  
-  
-  
+
+// // El punto se moverá según se mueva la línea de tiempo de la parte inferior. Para fijar la cámara al punto en movimiento hacemos:
+viewer.trackedEntity = satellitePoint;
